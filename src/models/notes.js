b70a7d571ef2032fbe066947a57e067373129183
sahithvibudhi <v.sahithkumar@gmail.com>
2019-10-20 03:39:16 +0530

    notes endpoint added

--- src/models/notes.js
+++ src/models/notes.js
@@ -2,6 +2,7 @@ const { mongoose } = require('./index');
 const Schema = mongoose.Schema;
 
 var notesSchema = new Schema({
+  name: { type: String, unique: false, required: true },
   body:  { type: String, unique : false, required : false },
   creator_id : { type: String, unique : false, index: true, required : true },
   created_at: { type: Date,  unique : false,  index: false, default : new Date() },
--- src/routers/notes.js
+++ src/routers/notes.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Notes = require('../models/notes');
 
 const router = express.Router();
 
-router.use('/', (req, res) => {
-    res.status(500).send('notes setup incomplete');
-})
+router.get('/', async (req, res) => {
+    const creator_id = res.locals.user.id;
+    try {
+        const q = { creator_id };
+        if (req.query.id) {
+            q._id = new mongoose.Types.ObjectId(req.query.id);
+        }
+        const r = await Notes.find(q);
+        res.send(r);
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+router.post('/', (req, res) => {
+    const creator = res.locals.user.id;
+    const name = req.body.name;
+    const body = req.body.body;
+    const label = req.body.label;
+    const team = req.body.label;
+    const notes = new Notes();
+    notes.creator_id = creator;
+    notes.name = name;
+    notes.body = body;
+    notes.label = label;
+    notes.team = team;
+    notes.label = label;
+    try {
+        notes.save();
+    } catch(e) {
+        return res.send(e);
+    }
+    res.status(201).send(notes);
+});
 
 module.exports = router;
