ccc0086e994e1af47c350501548071338737ceab
sahithvibudhi <v.sahithkumar@gmail.com>
2019-10-20 03:27:29 +0530

    teams endpoint

--- src/models/teams.js
+++ src/models/teams.js
@@ -2,7 +2,7 @@ const { mongoose } = require('./index');
 const Schema = mongoose.Schema;
 
 var teamSchema = new Schema({
-  owner:  { type: String, index: true, required : true },
+  creator_id:  { type: String, index: true, required : true },
   name: { type: String, index: false, required : true },
   created_at: { type: Date,  unique : false,  index: false, default : new Date() },
   members: { type: Array, default: [] }
--- src/routers/teams.js
+++ src/routers/teams.js
@@ -1,9 +1,37 @@
 const express = require('express');
 
+const Teams = require('../models/teams');
+
 const router = express.Router();
 
-router.use('/', (req, res) => {
-    res.status(500).send('teams setup incomplete');
-})
+router.get('/', async (req, res) => {
+    const creator_id = res.locals.user.id;
+    try {
+        const q = { creator_id };
+        if (req.query.name) {
+            q.name = req.query.name;
+        }
+        const r = await Teams.find(q);
+        res.send(r);
+    } catch (e) {
+        return res.status(500).send(e);
+    }
+});
+
+router.post('/', (req, res) => {
+    const creator = res.locals.user.id;
+    const name = req.body.name;
+    const members = req.body.members;
+    const team = new Teams();
+    team.creator_id = creator;
+    team.name = name;
+    team.members = members;
+    try {
+        team.save();
+    } catch(e) {
+        return res.send(e);
+    }
+    res.status(201).send(team);
+});
 
 module.exports = router;
