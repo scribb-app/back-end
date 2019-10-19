a8a17b7cd671e4ad81fd30a9bba8aa39c3c0ce34
sahithvibudhi <v.sahithkumar@gmail.com>
2019-10-19 20:17:14 +0530

    create label endpoint

--- src/routers/labels.js
+++ src/routers/labels.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
+const Labels = require('../models/labels');
+
 const router = express.Router();
 
-router.use('/', (req, res) => {
+router.get('/', (req, res) => {
     res.status(500).send('notes setup incomplete');
-})
+});
+
+router.post('/', (req, res) => {
+    const creator = res.locals.user.id;
+    const name = req.body.name;
+    const label = new Labels();
+    label.creator_id = creator;
+    label.name = name;
+    try {
+        label.save();
+    } catch(e) {
+        return res.send(e);
+    }
+    res.status(201).json(label.toJSON());
+});
 
 module.exports = router;
